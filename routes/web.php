9d6be727e369d040f2e0682688ad0d63da7fa308
nakamura <nakamurag70@gmail.com>
2020-07-22 14:56:48 +0000

    follow, unfollow

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MicropostsController@index');
 
 // ユーザ登録
 Route::get('signup', 'Auth\RegisterController@showregistrationForm')->name('signup.get');
@@ -25,10 +23,16 @@ Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 Route::group(['middleware' => ['auth']], function () { //ログインしていないとつかえないぞって意味合い
+    Route::group(['prefix' => 'users/{id}'], function () {
+    Route::post('follow', 'UserFollowController@store')->name('user.follow');
+    Route::delete('unfollow', 'UserFollowController@destroy')->name('user.unfollow');
+    Route::get('followings', 'UsersController@followings')->name('users.followings');
+    Route::get('followers', 'UsersController@followers')->name('users.followers');
+    });
+    // ユーザ一覧とユーザ詳細のみ
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+
+    // 登録と削除のみ
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
 
-// ユーザ一覧とユーザ詳細
-Route::group(['middleware' => ['auth']], function () {
-    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
-});
